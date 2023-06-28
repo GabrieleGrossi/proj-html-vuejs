5a78b0d83c62d2cbd8205005d357c0fe5a5eca2f
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-28 17:56:01 +0200

    Add flip card and change btn

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -13,51 +13,103 @@
         </div>
         <div class="flex card-section">
             <div class="card">
-                <div class="circle">
-                    <i class="fa-regular fa-building"></i>
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="circle">
+                            <i class="fa-regular fa-building"></i>
+                        </div>
+                        <h2>
+                            Buildings
+                        </h2>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum. 
+                        </p>
+                    </div>
+                    <div class="card-back">
+                        <h3>
+                            Artfully Crafted
+                        </h3>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.
+                        </p>
+                        <button class="get-quote">GET A QUOTE</button>
+                    </div>
                 </div>
-                <h2>
-                    Buildings
-                </h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, laborum perspiciatis. 
-                </p>
             </div>
 
             <div class="card">
-                <div class="circle">
-                    <i class="fa-solid fa-arrows-rotate"></i>
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="circle">
+                            <i class="fa-solid fa-arrows-rotate"></i>
+                        </div>
+                        <h2>
+                            Renovate
+                        </h2>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum. 
+                        </p>
+                    </div>
+                    <div class="card-back">
+                        <h3>
+                            Freshly New
+                        </h3>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.
+                        </p>
+                        <button class="get-quote"> GET A QUOTE</button>
+                    </div>
                 </div>
-                <h2>
-                    Renovate
-                </h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, laborum perspiciatis. 
-                </p>
             </div>
 
             <div class="card">
-                <div class="circle">
-                    <i class="fa-solid fa-house"></i>
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="circle">
+                            <i class="fa-solid fa-house"></i>
+                        </div>
+                        <h2>
+                            Construct
+                        </h2>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum. 
+                        </p>
+                    </div>
+                    <div class="card-back">
+                        <h3>
+                            Perfect Lines
+                        </h3>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.
+                        </p>
+                        <button class="get-quote">GET QUOTE</button>
+                    </div>
                 </div>
-                <h2>
-                    Construct
-                </h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, laborum perspiciatis. 
-                </p>
             </div>
 
             <div class="card">
-                <div class="circle">
-                    <i class="fa-solid fa-truck"></i>
+                <div class="card-inner">
+                    <div class="card-front">
+                        <div class="circle">
+                            <i class="fa-solid fa-truck"></i>
+                        </div>
+                        <h2>
+                            Exclusive
+                        </h2>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.
+                        </p>
+                    </div>
+                    <div class="card-back">
+                        <h3>
+                            Planning
+                        </h3>
+                        <p>
+                            Quis autem vel eum iure reprederit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum.
+                        </p>
+                        <button class="get-quote">GET A QUOTE</button>
+                    </div>
                 </div>
-                <h2>
-                    Exclusive
-                </h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, laborum perspiciatis. 
-                </p>
             </div>
         </div>
     </section>
@@ -89,12 +141,42 @@ export default {
     height: 300px;
     width: calc((100% / 4) - 2rem);
     background-color: #f5f5f5;
-    padding: 0 1rem;
     margin: 0 auto;
     border-radius: 15px;
     text-align: center;
+    perspective: 1000px;
    }
-   div.circle i{
+   .card-inner{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    border-radius: 15px;
+    text-align: center;
+    transition: transform 0.2s;
+    transform-style: preserve-3d;
+   }
+   .card:hover .card-inner{
+    transform: rotateY(180deg);
+    background-color: #fed03d;
+   }
+   .card-front, .card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    }
+
+    .card-back{
+    transform: rotateY(180deg);
+    }
+    .card-back h3, button{
+        margin-top: 4rem;
+    }
+    .card-back button{
+        background-color: white;
+    }
+    div.circle i{
     font-size: xx-large;
    }
    div.card p{
